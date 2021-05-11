668dc3a650428fab3e164bed5f6fecda13603286
lhcopetti <lhcopetti@gmail.com>
2021-05-11 19:50:44 -0300

    Remove some unused lines of code from contentScript.ts

--- contentScript.ts
+++ contentScript.ts
@@ -1,11 +1,3 @@
-
-export function main() {
-    console.log(
-        "Is chrome.runtime available here?",
-        typeof chrome.runtime.sendMessage == "function",
-    );
-};
-
 import LibNotation from './src/AlgebraicNotation'
 import LibHtml from './src/HtmlController'
 import LichessKeyboard from './src/LichessKeyboard'
@@ -17,20 +9,20 @@ console.log("Initializing LichessKeyboard extension");
 
 const lichessBoardReader = new LichessHtmlBoardReader();
 const htmlController = new LibHtml(document, lichessBoardReader);
-const libNotation = new LibNotation();
 
 const lichessAPIClient = new LichessAPIClient();
 const tokenStorage = new TokenStorage();
 
-const lichessKeyboard = new LichessKeyboard(libNotation, lichessAPIClient);
+const lichessKeyboard = new LichessKeyboard(new LibNotation(), lichessAPIClient);
 
 tokenStorage.addTokenChangedListener(newToken => {
-    console.log("TokenChangedListener called with: " + newToken);
     lichessAPIClient.updateToken(newToken);
 });
 
-htmlController.addListener(lichessKeyboard);
+tokenStorage.getToken().then(token => lichessAPIClient.updateToken(token));
 
+
+htmlController.addListener(lichessKeyboard);
 htmlController.init();
 
 chrome.runtime.sendMessage({ data: "URL_REQUEST" } , function(response) {
@@ -40,10 +32,3 @@ chrome.runtime.sendMessage({ data: "URL_REQUEST" } , function(response) {
     lichessKeyboard.updateGameId(gameId);
 });
 
-setTimeout(() => {
-   const htmlBoard = lichessBoardReader.readBoard(document);
-   console.log(htmlBoard);
-}, 1000);
-
-
-tokenStorage.getToken().then(token => lichessAPIClient.updateToken(token));
